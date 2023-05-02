876496a4dd92de8b7a6c484d580a90a619038dce
Azim Chowdhury <azimchy994@gmail.com>
2023-05-02 21:40:57 +0800

    interface

--- 2-Advanced TypeScript/src/main.ts
+++ 2-Advanced TypeScript/src/main.ts
@@ -4,17 +4,46 @@ let emergency: any;
 emergency = 999;
 
 const bucket = (emergency as number).toString()
-// console.log(typeof bucket);
+// different syntax of type assertion
+const bucket2 = <number>emergency
+// console.log(typeof bucket, typeof bucket2);
 
 function kgToGram(input: number | string): string | number | undefined {
-    if(typeof input === 'number'){
-        return input*1000
-    }else if(typeof input ==='string'){
-        const value = parseFloat(input)*1000;
+    if (typeof input === 'number') {
+        return input * 1000
+    } else if (typeof input === 'string') {
+        const value = parseFloat(input) * 1000;
         return `${value} gram`
     }
 }
 
 const tryIt = kgToGram('5') as string
 const tryIt2 = kgToGram(5) as number
-console.log(tryIt, tryIt2);
+// console.log(tryIt, tryIt2);
+
+
+// Interface
+interface IUser {
+    name: string;
+    class: number;
+    roll: number;
+}
+
+interface INewUser extends IUser {
+    section:string
+}
+
+const newUser: IUser = {
+    name: 'habib',
+    class: 9,
+    roll: 10, 
+}
+
+
+const newUser2 : INewUser ={
+    name:'rakib',
+    class:8,
+    roll:3,
+    section:'B'
+}
+ 
